abe2b1f21c2ee1af39b06dbafeec0ee963c0d8b8
Tac <fmessina@student.42.fr>
2019-02-04 20:30:13 +0100

    feat: added firt batch of source file

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "scop.h"
+
+int main (int ac, char **av)
+{
+    if (ac > 1)
+    {
+        ft_putstr(av[1]);
+    }
+    ft_putstr("\ni'm in!\n\n");
+    while (1)
+    {
+        ;
+    }
+    ft_putstr("i'm out!\n");
+    return (0);
+}
