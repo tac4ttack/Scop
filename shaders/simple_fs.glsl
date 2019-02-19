8a1647ab1cb6ddb2e29be2d4a39dbf4cf8bf1107
Tac <fmessina@student.42.fr>
2019-02-19 20:06:19 +0100

    shader: Updated the simple example vertex and fragment shaders

--- shaders/simple_fs.glsl
+++ shaders/simple_fs.glsl
@@ -1,5 +1,14 @@
-#version 430 core
-out vec3 color;
-void main(){
-  color = vec3(1,0,0);
+// #version 430 core
+// out vec3 color;
+// void main(){
+//   color = vec3(1,0,0);
+// }
+
+#version 420
+
+uniform vec4 inputColour;
+out vec4 fragColour;
+
+void main() {
+  fragColour = inputColour;
 }
--- shaders/simple_vs.glsl
+++ shaders/simple_vs.glsl
@@ -1,6 +1,15 @@
-#version 330 core
-layout(location = 0) in vec3 vertexPosition_modelspace;
-void main(){
-  gl_Position.xyz = vertexPosition_modelspace;
-  gl_Position.w = 1.0;
+// #version 330 core
+// layout(location = 0) in vec3 vertexPosition_modelspace;
+// void main(){
+//   gl_Position.xyz = vertexPosition_modelspace;
+//   gl_Position.w = 1.0;
+// }
+
+
+#version 420
+
+in vec3 vertex_position;
+
+void main() {
+  gl_Position = vec4(vertex_position, 1.0);
 }
