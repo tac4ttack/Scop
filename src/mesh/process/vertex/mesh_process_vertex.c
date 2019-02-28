cbe9d8e9ce0bf5f7ca4d38655617a6466189cd6e
Tac <fmessina@student.42.fr>
2019-02-28 18:41:13 +0100

    feat: OBJ vertex parsing done

--- src/mesh/process/vertex/mesh_process_vertex.c
+++ src/mesh/process/vertex/mesh_process_vertex.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 15:58:21 by fmessina          #+#    #+#             */
-/*   Updated: 2019/02/27 18:18:12 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/02/28 17:57:58 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,53 @@ static bool		create_v_array(t_mesh *mesh)
 {
 	if (mesh)
 	{
-		if (!(mesh->vertex = ft_memalloc(sizeof(float) \
+		if (!(mesh->vertex = ft_memalloc(sizeof(GLfloat) \
 										* 8 * mesh->n_vertex[0])))
-			return ((error_bool("[ERROR create_v_array]\t" \
-			"Mesh vertex array memory allocation failed!\n")));
+			return (error_bool("[ERROR create_v_array]\t" \
+			"Mesh vertex array memory allocation failed!\n"));
 		return (true);
 	}
-	return ((error_bool("[ERROR create_v_array]\t" \
-	"NULL mesh pointer!\n")));
+	return (error_bool("[ERROR create_v_array]\t" \
+	"NULL mesh pointer!\n"));
+}
+
+static bool		fill_vertex_hue(t_mesh *mesh, const int index)
+{
+	if (mesh)
+	{
+		mesh->vertex[index + 4] = 1.0f;
+		mesh->vertex[index + 5] = 0.0f;
+		mesh->vertex[index + 6] = 1.0f;
+		mesh->vertex[index + 7] = 1.0f;
+		return (true);
+	}
+	return (error_bool("[ERROR fill_vertex_hue]\tNULL mesh pointer!\n"));
 }
 
 static bool		check_vertex_data(t_mesh *mesh, const int index, const int ret)
 {
-	if (mesh)
+	bool		res;
+
+	if (mesh && (res = true))
 	{
-		// NEEDS TO BE FINISHEDD!!!!!!
-		if (ret == 8)
-			return (true);
-		else if (ret == 3)	// 3 comp without color
+		if (res && ret == 3)
 		{
 			mesh->vertex[index + 3] = 1.0f;
-			mesh->vertex[index + 4] = 0.42f;
-			mesh->vertex[index + 5] = 0.0f;
-			mesh->vertex[index + 6] = 0.70f;
-			mesh->vertex[index + 7] = 1.0f;
+			res = fill_vertex_hue(mesh, index);
 		}
-		else if (ret == 4)	// 4 comp without color
-		{
-			mesh->vertex[index + 4] = 0.42f;
-			mesh->vertex[index + 5] = 0.0f;
-			mesh->vertex[index + 6] = 0.70f;
+		else if (res && ret == 4)
+			res = fill_vertex_hue(mesh, index);
+		else if (res && ret == 7)
 			mesh->vertex[index + 7] = 1.0f;
-		}
-		else if (ret == 7)	// 4 comp without color
-			mesh->vertex[index + 7] = 1.0f;
-		else
-			return ((error_bool("[ERROR check_vertex_data]\t" \
-			"Vertex line format is incorrect\n" \
-			"It needs to be X Y Z W R G B A\n" \
-			"XYZ are mandatory, if you put color you need to specify at least the RGB!\n")));
-		return (true);
+		else if (!res && ret != 8)
+			return (error_bool("[ERROR check_vertex_data]\tVertex line forma" \
+			"t is incorrect\n It needs to be X Y Z W R G B A\nXYZ are mandato" \
+			"ry, if you put color you need to specify at least the RGB!\n"));
+		return (res);
 	}
-	return ((error_bool("[ERROR check_vertex_data]\t" \
-	"NULL mesh pointer!\n")));
+	return (error_bool("[ERROR check_vertex_data]\t" \
+	"NULL mesh pointer!\n"));
 }
-
 
 bool	mesh_process_vertex(t_mesh *mesh, char *str)
 {
@@ -79,13 +81,13 @@ bool	mesh_process_vertex(t_mesh *mesh, char *str)
 					&mesh->vertex[i[0] + 4], &mesh->vertex[i[0] + 5], \
 					&mesh->vertex[i[0] + 6], &mesh->vertex[i[0] + 7]);
 		if (i[1] < 3)
-			return (!scop_log_err("[ERROR mesh_process_vertex]\t" \
-				"Missing value in vertex definition line!\n", str));
+			return (error_bool("[ERROR mesh_process_vertex]\t" \
+				"Missing value in vertex definition line!\n"));
 		if (!(check_vertex_data(mesh, i[0], i[1])))
 			return (error_bool("[ERROR mesh_process_vertex]\t" \
 				"Missing values in vertex definition line!\n"));
 		return (true);
 	}
-	return ((error_bool("[ERROR mesh_process_vertex]\t" \
-	"NULL mesh or string pointer!\n")));
+	return (error_bool("[ERROR mesh_process_vertex]\t" \
+	"NULL mesh or string pointer!\n"));
 }
