90847514351352b1e9e4ac62d45170acc1ebe322
Tac <fmessina@student.42.fr>
2019-02-28 18:47:43 +0100

    fix: Removed VAO packing call from mesh file processing, it is now called in the main()

--- src/mesh/mesh_file_process.c
+++ src/mesh/mesh_file_process.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 14:15:07 by fmessina          #+#    #+#             */
-/*   Updated: 2019/02/27 18:52:45 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/02/28 18:09:40 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,7 @@ t_mesh *mesh_file_process(t_scop *env)
 		if (!mesh_line_process(mesh, split))
 			return (mesh_file_processing_error(mesh, split, \
 			"[ERROR mesh_file_process]\tMesh line processing failed!\n"));
-		//(DEBUG_SCOP ? mesh_print_data(mesh) : 0); //
-		if (!mesh_pack_vao_data(mesh))
-			return (mesh_file_processing_error(mesh, split, \
-			"[ERROR mesh_file_process]\tVAO data packing failed!\n"));
+		(DEBUG_SCOP ? mesh_print_data(mesh) : 0);
 		split_destroy(split);
 		scop_log("Finished mesh processing with success!\n");
 		return (mesh);
