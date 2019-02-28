c96c14e91e70ae7736bf0a3cee6e728249b169e1
Tac <fmessina@student.42.fr>
2019-02-28 18:44:23 +0100

    fix: Mesh cleaning updated with the VAO data pack

--- src/mesh/mesh_clean.c
+++ src/mesh/mesh_clean.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 12:21:33 by fmessina          #+#    #+#             */
-/*   Updated: 2019/02/27 18:21:39 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/02/28 12:17:18 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@ void	mesh_clean(t_mesh *mesh)
 	if (mesh)
 	{
 		scop_log("Cleaning mesh data...", NULL);
-		(mesh->object ? ft_memdel((void**)&mesh->object) : 0);
-		(mesh->group ? ft_memdel((void**)&mesh->group) : 0);
-		(mesh->mtllib ? ft_memdel((void**)&mesh->mtllib) : 0);
-		(mesh->usemtl ? ft_memdel((void**)&mesh->usemtl) : 0);
+		// (mesh->object ? ft_memdel((void**)&mesh->object) : 0);
+		// (mesh->group ? ft_memdel((void**)&mesh->group) : 0);
+		// (mesh->mtllib ? ft_memdel((void**)&mesh->mtllib) : 0);
+		// (mesh->usemtl ? ft_memdel((void**)&mesh->usemtl) : 0);
 		(mesh->vertex ? ft_memdel((void**)&mesh->vertex) : 0);
 		(mesh->face ? ft_memdel((void**)&mesh->face) : 0);
 		(mesh->normal ? ft_memdel((void**)&mesh->normal) : 0);
 		(mesh->texture ? ft_memdel((void**)&mesh->texture) : 0);
 		(mesh->space ? ft_memdel((void**)&mesh->space) : 0);
 		(mesh->face_format ? ft_memdel((void**)&mesh->face_format) : 0);
+		(mesh->final_vao ? ft_memdel((void**)&mesh->final_vao) : 0);
 		(mesh ? ft_memdel((void**)&mesh) : 0);
 	}
 }
