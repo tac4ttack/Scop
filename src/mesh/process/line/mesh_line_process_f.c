f8c2d81d8306eaf76f4133a7aa05e37c3bc652b1
Tac <fmessina@student.42.fr>
2019-02-28 18:46:46 +0100

    fix: Function mesh_line_process_f() fixed, it was missing a return true causing its caller consider it failed everytime

--- src/mesh/process/line/mesh_line_process_f.c
+++ src/mesh/process/line/mesh_line_process_f.c
@@ -6,11 +6,12 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 12:59:37 by fmessina          #+#    #+#             */
-/*   Updated: 2019/02/27 18:10:26 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/02/28 15:56:57 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
+
 
 bool	mesh_line_process_f(t_mesh *mesh, char *str)
 {
@@ -22,6 +23,7 @@ bool	mesh_line_process_f(t_mesh *mesh, char *str)
 		if (!(mesh_process_face(mesh, str)))
 			return (!scop_log_err("[ERROR mesh_line_process_f]\t" \
 			"Face element line processing failed ->\t%s\n", str));
+		return (true);
 	}
 	return (error_bool("[ERROR mesh_line_process_f]\t"\
 	"NULL mesh or string pointer!\n"));
