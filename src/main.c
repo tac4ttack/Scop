6b3d039a868298f420ec81b523df761fc5432f94
Tac <fmessina@student.42.fr>
2019-02-28 18:44:48 +0100

    feat: Added the VAO packing process into the main flow

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 14:05:26 by fmessina          #+#    #+#             */
-/*   Updated: 2019/02/25 15:49:42 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/02/28 11:50:22 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ int main(int ac, char **av)
 			exit_fail("Could not load targeted mesh file :(", env);
 		if (!(env->mesh = mesh_file_process(env)))
 			exit_fail("Invalid OBJ format for targeted mesh file :(", env);
+		if (!(mesh_pack_vao_data(env->mesh)))
+			exit_fail("Failed to pack data for OpenGL :(", env);
 		if (!(shader_build(env)))
 			exit_fail("Could create the shader program :(", env);
 		if (!(buffer_create(env)))
 			exit_fail("Could create vertex buffers :(", env);
 		if (!(glfw_launch(env)))
 			exit_fail("Exception caught! :(", env);
-
-
 
 
 		exit_ok(env);
