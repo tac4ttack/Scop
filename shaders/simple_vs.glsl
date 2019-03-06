7c2b543c22c9095191ce408423f2322f22ce815b
Tac <fmessina@student.42.fr>
2019-03-06 16:02:09 +0100

    feat: Added new location to vertex shader

--- shaders/simple_vs.glsl
+++ shaders/simple_vs.glsl
@@ -1,6 +1,9 @@
 #version 330 core
 layout (location = 0) in	vec4	aPos;
 layout (location = 1) in	vec4	aHue;
+layout (location = 2) in	vec3	aTexture;
+layout (location = 3) in	vec3	aNormal;
+layout (location = 4) in	vec3	aSpace;
 out 						vec4	vertexColor;
 void main()
 {
