d0decd269c08807d4028ed6ee7f3a3ec42e16560
Tac <fmessina@student.42.fr>
2019-03-06 13:37:06 +0100

    fix: Moved the mesh debug value output into the main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 14:05:26 by fmessina          #+#    #+#             */
-/*   Updated: 2019/02/28 11:50:22 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/03/06 12:26:21 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int main(int ac, char **av)
 			exit_fail("Invalid OBJ format for targeted mesh file :(", env);
 		if (!(mesh_pack_vao_data(env->mesh)))
 			exit_fail("Failed to pack data for OpenGL :(", env);
+		(DEBUG_SCOP ? mesh_print_data(env->mesh) : 0);
 		if (!(shader_build(env)))
 			exit_fail("Could create the shader program :(", env);
 		if (!(buffer_create(env)))
 			exit_fail("Could create vertex buffers :(", env);
 		if (!(glfw_launch(env)))
 			exit_fail("Exception caught! :(", env);
-
 
 		exit_ok(env);
 	}
