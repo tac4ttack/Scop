f7fc8bebd6e52fa0a126a0618548d7272ba50f6d
Tac <fmessina@student.42.fr>
2019-03-09 16:43:10 +0100

    feat: Resizing the window is recomputing the viewport

--- src/glfw/glfw_window_size_callback.c
+++ src/glfw/glfw_window_size_callback.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 11:40:21 by fmessina          #+#    #+#             */
-/*   Updated: 2019/03/08 13:38:02 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/03/09 12:23:27 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ void	glfw_window_size_callback(GLFWwindow *win, \
 									const int width, \
 									const int height)
 {
+	t_scop *env;
+
+	env = glfwGetWindowUserPointer(win);
 	if (win)
 	{
-		glViewport(0, 0, width, height);
+		env->win_res[0] = width;
+		env->win_res[1] = height;
+		env->win_res[2] = env->win_res[0] / env->win_res[1];
+		glViewport(0, 0, env->win_res[0], env->win_res[1]);
 		if (DEBUG_SCOP)
 		{
 			// add a print of a glGet(GL_VIEWPORT, param)
 			scop_log("\nWindow size has changed!\tnew width = " \
-					"%d\tnew height = %d\n", width, height);
+					"%d\tnew height = %d\n", env->win_res[0], env->win_res[1]);
 		}
 	}
 }
