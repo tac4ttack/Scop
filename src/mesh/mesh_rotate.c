3a49274770c6a598a5991332d51ccf200e33f5d2
Tac <fmessina@student.42.fr>
2019-03-15 15:06:40 +0100

    dev: Removed useless crap?

--- src/mesh/mesh_rotate.c
+++ src/mesh/mesh_rotate.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 10:49:59 by fmessina          #+#    #+#             */
-/*   Updated: 2019/03/14 13:05:17 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/03/15 14:44:18 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ bool	mesh_rotate_self(t_scop *env, int key)
 			env->world->mesh_euler[2] += 1.0;
 		else if (key == GLFW_KEY_F15)
 			env->world->mesh_euler[2] -= 1.0;
-		env->world->mesh_euler[0] = (env->world->mesh_euler[0] < 0.0 ? 359.0 : env->world->mesh_euler[0]);
-		env->world->mesh_euler[0] = (env->world->mesh_euler[0] > 360.0 ? 1.0 : env->world->mesh_euler[0]);
-		env->world->mesh_euler[1] = (env->world->mesh_euler[1] < 0.0 ? 359.0 : env->world->mesh_euler[1]);
-		env->world->mesh_euler[1] = (env->world->mesh_euler[1] > 360.0 ? 1.0 : env->world->mesh_euler[1]);
-		env->world->mesh_euler[2] = (env->world->mesh_euler[2] < 0.0 ? 359.0 : env->world->mesh_euler[2]);
-		env->world->mesh_euler[2] = (env->world->mesh_euler[2] > 360.0 ? 1.0 : env->world->mesh_euler[2]);
+		// env->world->mesh_euler[0] = (env->world->mesh_euler[0] < 0.0 ? 359.0 : env->world->mesh_euler[0]);
+		// env->world->mesh_euler[0] = (env->world->mesh_euler[0] > 360.0 ? 1.0 : env->world->mesh_euler[0]);
+		// env->world->mesh_euler[1] = (env->world->mesh_euler[1] < 0.0 ? 359.0 : env->world->mesh_euler[1]);
+		// env->world->mesh_euler[1] = (env->world->mesh_euler[1] > 360.0 ? 1.0 : env->world->mesh_euler[1]);
+		// env->world->mesh_euler[2] = (env->world->mesh_euler[2] < 0.0 ? 359.0 : env->world->mesh_euler[2]);
+		// env->world->mesh_euler[2] = (env->world->mesh_euler[2] > 360.0 ? 1.0 : env->world->mesh_euler[2]);
 		return (true);
 	}
 	return (error_bool("[ERROR mesh_rotate_self]\tNULL Scop pointer!\n"));
