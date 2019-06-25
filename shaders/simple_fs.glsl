65ce58b6314ca3f3ccf8b08a22984005b6639dd6
Florian MESSINA <fmessina@e3r3p19.42.fr>
2019-06-25 16:34:16 +0200

    feat: Shaders have now flat and smooth colors, for test purposes at the moment

--- shaders/simple_fs.glsl
+++ shaders/simple_fs.glsl
@@ -1,17 +1,32 @@
 #version 330 core
-out			vec4		FragColor;
-in			vec4		vertexColor;
-in			vec2		texCoord;
-in			vec3		vertexNormal;
+out vec4 FragColor;
+
+smooth in vec4 vertexColor;
+flat in vec4 vertexColorFlat;
+
+in vec2 texCoord;
+in vec2	texCoordDefault;
+in vec3 vertexNormal;
+in vec4	vertexNormalDefault;
+
 
 uniform		sampler2D	defaultTexture;
 void main()
 {
-	// FragColor = vertexColor;
-	// FragColor = texture(defaultTexture, texCoord) * vertexColor;
 	FragColor = texture(defaultTexture, texCoord);
-	// FragColor = vec4(sin(timeVal) / vertexColor.x, \
-	//				cos(timeVal) / vertexColor.y, \
-	//				cos(timeVal) / vertexColor.z, \
-	//				vertexColor.w);
+	// FragColor = texture(defaultTexture, texCoordDefault);
+	// FragColor = vertexColorFlat;
+	// FragColor = vertexColor;
+	
+	// FragColor = texture(defaultTexture, texCoord) * vertexColorSmooth;
+	// FragColor = texture(defaultTexture, texCoord) * vertexColorFlat;
+	
+	// FragColor = vec4(sin(timeVal) / vertexColorSmooth.x, \
+	// FragColor = vec4(sin(timeVal) / vertexColorSmooFlat\
+	//				cos(timeVal) / vertexColorSmooth.y, \
+	//				cos(timeVal) / vertexColorSmooFlat\
+	//				cos(timeVal) / vertexColorSmooth.z, \
+	//				cos(timeVal) / vertexColorSmooFlat\
+	//				vertexColorSmooth.w);
+	//				vertexColorSmoFlat;
 }
