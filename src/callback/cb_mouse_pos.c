6cf204c59da1e85bc259adcc7544bc2dd8190532
Florian MESSINA <fmessina@e1r9p19.42.fr>
2019-07-01 12:28:18 +0200

    feat: Finished mouse control

--- src/callback/cb_mouse_pos.c
+++ src/callback/cb_mouse_pos.c
@@ -6,11 +6,23 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 11:25:42 by fmessina          #+#    #+#             */
-/*   Updated: 2019/07/01 11:30:30 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/07/01 12:27:46 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
+
+static void	fill_mouse_pos(t_scop *env, float x, float y, float *dst)
+{
+	dst[0] = env->mouse->rot_speed * \
+		(2.0f * env->mouse->last[0] - env->win_res[0]) / (float)env->win_res[0];
+	dst[1] = env->mouse->rot_speed * \
+		(env->win_res[1] - 2.0f * env->mouse->last[1]) / (float)env->win_res[1];
+	dst[2] = env->mouse->rot_speed * \
+		(2.0f * (float)x - env->win_res[0]) / (float)env->win_res[0];
+	dst[3] = env->mouse->rot_speed * \
+		(env->win_res[1] - 2.0f * (float)y) / (float)env->win_res[1];
+}
 
 void		cb_mouse_pos(GLFWwindow *window, \
 						double xpos, \
@@ -20,19 +32,24 @@ void		cb_mouse_pos(GLFWwindow *window, \
 	float	mouse_pos[4];
 
 	env = glfwGetWindowUserPointer(window);
-	mouse_pos[0] = env->mouse->rot_speed * \
-		(2.0f * env->mouse->last[0] - env->win_res[0]) / (float)env->win_res[0];
-	mouse_pos[1] = env->mouse->rot_speed * \
-		(env->win_res[1] - 2.0f * env->mouse->last[1]) / (float)env->win_res[1];
-	mouse_pos[2] = env->mouse->rot_speed * \
-		(2.0f * (float)xpos - env->win_res[0]) / (float)env->win_res[0];
-	mouse_pos[3] = env->mouse->rot_speed * \
-		(env->win_res[1] - 2.0f * (float)ypos) / (float)env->win_res[1];
+	fill_mouse_pos(env, xpos, ypos, (float*)&mouse_pos);
 	if (env->mouse->lmb)
 	{
 		input_mouse_rot(&env->mouse->prev, mouse_pos);
 		env->world->mesh_orient = quat_mul(env->world->mesh_orient, \
 											env->mouse->prev);
+	}
+	else if (env->mouse->rmb)
+	{
+		env->world->mesh_position.x += env->mouse->tra_speed * \
+			((float)xpos - env->mouse->last[0]) / (float)env->win_res[0];
+		env->world->mesh_position.y -= env->mouse->tra_speed * \
+			((float)ypos - env->mouse->last[1]) / (float)env->win_res[1];
+	}
+	else if (env->mouse->mmb)
+	{
+		env->world->mesh_position.z -= env->mouse->tra_speed * \
+			((float)ypos - env->mouse->last[1]) / (float)env->win_res[1];
 	}
 	env->mouse->last[0] = xpos;
 	env->mouse->last[1] = ypos;
--- src/init/init_mouse.c
+++ src/init/init_mouse.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 10:28:09 by fmessina          #+#    #+#             */
-/*   Updated: 2019/07/01 10:14:05 by fmessina         ###   ########.fr       */
+/*   Updated: 2019/07/01 12:27:29 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,9 @@ bool	init_mouse(t_scop *env)
 			"memory for the mouse support!\n"));
 		env->mouse->ready = false;
 		env->mouse->rot_speed = 1.0f;
-		env->mouse->tra_speed = 2.0f;
+		env->mouse->tra_speed = 4.0f;
 		env->mouse->last[0] = (double)env->win_res[0] / 2.0;
 		env->mouse->last[1] = (double)env->win_res[1] / 2.0;
-		env->mouse->dummy[0] = 0.0f;
-		env->mouse->dummy[1] = 0.0f;
-		env->mouse->dummy[2] = 0.0f;
-		env->mouse->dummy[3] = 0.0f;
 		return (true);
 	}
 	return (error_bool("[ERROR init_mouse]\tNULL scop pointer!\n"));
